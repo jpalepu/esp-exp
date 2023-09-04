fbcb7f65c7cb0fb6786a425d4e5fa4e8d5844b95
Jithin Palepu <44967770+jpalepu@users.noreply.github.com>
2023-09-04 10:44:02 +0200

    Update esp-exp.c
    all details on turning on the deep sleep and light sleep modes

--- main/esp-exp.c
+++ main/esp-exp.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-void app_main(void)
-{
+#include "esp_system.h"
+#include "esp_sleep.h"
 
+void app_main() {
+    printf("ESP32 Sleep Mode Test\n");
+
+    while (1) {
+        printf("Running in active mode...\n");
+
+        // Perform your application tasks here in active mode.
+        // all of our custom tasks can be plced here. 
+
+        // Delay for a few seconds before entering sleep mode.
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        printf("Entering light sleep mode...\n");
+
+        // light sleep 
+        esp_light_sleep_start();
+
+        // After waking up from light sleep, execution continues here.
+        // You can add code to restore the device's state if necessary.
+        printf("Waking up from light sleep mode...\n");
+
+        // Delay for a few seconds before entering deep sleep mode.
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        printf("Entering deep sleep mode...\n");
+
+        //  timer for deep sleep (ex 10 (in microseconds)seconds).
+        esp_sleep_enable_timer_wakeup(10 * 1000000);
+
+        // entering deep sleep mode and wake up after 10 seconds.
+        esp_deep_sleep_start();
+
+        // after exiting the deep sleep mode, hthe program will again start from the aapp main. becuse the device is going to be reset after the deep sleep release. 
+      // maybe w can configure some external interrupt pin to exit from the deep sleep mode. 
+    }
 }
